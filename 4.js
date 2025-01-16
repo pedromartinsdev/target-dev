a84a0a42f6136fcb03553b2bcc06aa29f6a1d875
Pedro Henrique Martins <59265145+pedromartinsdev@users.noreply.github.com>
2025-01-15 22:31:46 -0300

    Create 4.js

---
+++ 4.js
@@ -0,0 +1,22 @@
+/* 
+4) Dado o valor de faturamento mensal de uma distribuidora, detalhado por estado:
+• SP – R$67.836,43
+• RJ – R$36.678,66
+• MG – R$29.229,88
+• ES – R$27.165,48
+• Outros – R$19.849,53
+*/
+
+const sp = 67836.43
+const rj = 36678.66
+const mg = 29229.88
+const es = 27165.48
+const outros = 19849.53
+
+let total = sp + rj + mg + es + outros
+
+percentualSp = sp/total
+percentualRj = rj/total
+percentualMg = mg/total
+percentualEs = es/total
+percentualOutros = outros/total
